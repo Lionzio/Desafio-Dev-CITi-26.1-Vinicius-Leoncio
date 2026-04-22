4f20b41c0a94a9d2733d33fe819198d1e5db6c63
guga1610 <gjcln@cin.ufpe.br>
2026-04-22 10:37:59 -0300

    Remove UserController and associated routes

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -3,9 +3,5 @@ import userController from "./controllers/UserController";
 
 const routes = express.Router();
 
-routes.post("/user", userController.create);
-routes.get("/user", userController.get);
-routes.delete("/user/:id", userController.delete);
-routes.patch("/user/:id", userController.update);
 
 export default routes;
