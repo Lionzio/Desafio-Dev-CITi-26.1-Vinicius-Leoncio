77915bb2cafb9330f8770f25e30c5b333c5e2fd5
guga1610 <gjcln@cin.ufpe.br>
2026-04-23 11:23:14 -0300

    Add Calcado model migration and implement user retrieval endpoint

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,7 +1,10 @@
 import express from "express";
-import userController from "./controllers/UserController";
+import {  readAllUsers } from "./controllers/UserController";
+
 
 const routes = express.Router();
+
+routes.get("/users", readAllUsers);
 
 
 export default routes;
